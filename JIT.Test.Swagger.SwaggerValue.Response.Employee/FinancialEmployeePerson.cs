76d8d92a11144ba4ce6f4408cd6b9fcadbdc49d3
jitendra <jitsingh89@hotmail.com>
2018-01-10 22:45:19 +0530

    first commit

---
+++ JIT.Test.Swagger.SwaggerValue.Response.Employee/FinancialEmployeePerson.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JIT.Test.Swagger.SwaggerValue.Response.Employee
+{
+    public class Friend
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class FinancialEmployeePerson
+    {
+        public string _id { get; set; }
+        public int BackgroundProcessingImagefilepicturetestfile { get; set; }
+        public string BackgroundProcessingImagefilepicture1111111sssss { get; set; }
+        public bool BackgroundProcessingImagefilepicture12345 { get; set; }
+        public string baBackgroundProcessingImagefilepictureBackgroundProcessingImagefilepicturelance { get; set; }
+        public string BackgroundProcessingImagefilepicture { get; set; }
+        public int age { get; set; }
+        public string eyeColor { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string company { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string about { get; set; }
+        public string registered { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public List<string> tags { get; set; }
+        public List<Friend> friends { get; set; }
+        public string greeting { get; set; }
+        public string favoriteFruit { get; set; }
+    }
+}
---
+++ WebApi/Controllers/PersonController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+using Newtonsoft.Json.Converters;
+using System.Net.Http;
+using JIT.Test.Swagger.SwaggerValue.Response.Employee;
+
+namespace WebApi.Controllers
+{
+    public class PersonController : ApiController
+    {
+        /// <summary>
+        /// Save Persons 
+        /// </summary>
+        /// <param name="lst"> List of Employees. List of Employees. List of Employees. List of Employees. List of Employees. List of Employees. List of Employees. List of Employees.</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("api/Persons")]
+        public List<FinancialEmployeePerson> PostValues(List<FinancialEmployeePerson> lst)
+        {
+            return new List<FinancialEmployeePerson>();
+        }
+
+
+        /// <summary>
+        /// Save person 
+        /// </summary>
+        /// <param name="Employee">Employees data. Employees data.Employees data. Employees data. Employees data. Employees data. Employees data.</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("api/Person")]
+        public FinancialEmployeePerson PostValue(FinancialEmployeePerson lst)
+        {
+            return new FinancialEmployeePerson();
+        }
+
+    }
+}
